d0e3db5069ec617d7bcb816db01cd252bb6e173d
Pasi Suominen <pasiz@pasiz.net>
2013-01-24 17:11:17 +0200

    set high quality cookie

--- plugin.video.katsomo/resources/lib/katsomo.py
+++ plugin.video.katsomo/resources/lib/katsomo.py
@@ -1,11 +1,9 @@
-import cookielib
+from cookielib import LWPCookieJar, Cookie
 from urllib2 import HTTPError, URLError
 import urllib2,urllib,urlparse,re
-import CommonFunctions
+from CommonFunctions import common
 from datetime import datetime
 import time
-
-common = CommonFunctions
 
 class NetworkError(Exception):
 	pass
@@ -20,7 +18,7 @@ class katsomo():
 	}
 
 	def __init__(self, username="", password="", cookie_file=""):
-		self.cj = cookielib.LWPCookieJar(cookie_file)
+		self.cj = LWPCookieJar(cookie_file)
 		if username == "":
 			self.noLogin = True
 		else:
@@ -31,13 +29,34 @@ class katsomo():
 				self.cj.revert(ignore_discard = True)
 			except IOError:
 				pass
+		self.cj.set_cookie(self.makeCookie('hq','1'))
+		print self.cj
 		self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
 		self.user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
+	
+	def makeCookie(self, name, value):
+		return Cookie(
+		    version=0, 
+		    name=name, 
+		    value=value,
+		    port=None, 
+		    port_specified=False,
+		    domain="katsomo.fi", 
+		    domain_specified=True, 
+		    domain_initial_dot=False,
+		    path="/", 
+		    path_specified=True,
+		    secure=False,
+		    expires=None,
+		    discard=False,
+		    comment=None,
+		    comment_url=None,
+		    rest=None
+		)
 		
 	def getPage(self, url,postvars={}, header_data = {}):
 		req = urllib2.Request(url, urllib.urlencode(postvars), header_data)			
 		req.add_header('user-agent', self.user_agent)
-		#req.add_header('cookie
 		try:
 			resp = self.opener.open(req).read()
 		except HTTPError, error:
@@ -50,7 +69,6 @@ class katsomo():
 		if self.noLogin:
 			return False
 		ret = common.parseDOM(self.getPage(self.URL['login']), "div", attrs = { "class": "login" })
-		#ret = common.parseDOM(ret, "a", ret = "href")
 		if not "/katsomo/logout" in ret:
 			return self.doLogin()
 		else:
@@ -65,8 +83,6 @@ class katsomo():
 			'User-Agent' : self.user_agent,
 			'Referer' : 'http://m.katsomo.fi/katsomo/login'
 		}
-		
-		#ret = common.parseDOM(ret, "a", ret = "href")
 		if "/katsomo/logout" in common.parseDOM(self.getPage(self.URL['login'], postvars, header_data), "div", attrs = { "class": "login" }):
 			try:
 				self.cj.save( ignore_discard=True )
@@ -95,22 +111,18 @@ class katsomo():
 		
 	def getPrograms(self, prog_id): 
 		ret = common.parseDOM(self.getPage(self.URL['programs'] + prog_id), "div", {'class': 'program'})
-		
 		programs = []
 		for r in ret:
 			link = common.parseDOM(r, "a", ret = "href")[0]
 			title = common.parseDOM(r, "p", {'class': 'program-name'})[0]
 			if 'class="star"' in title and not self.getLoginStatus(): continue
 			elif 'class="star"' in title and self.getLoginStatus() and self.scrapVideoLink(link) == None: continue	
-			
 			title += ' ' + common.parseDOM(r, "p", {'class': 'program-abstract'})[0]
 			img = 'http://m.katsomo.fi' + common.parseDOM(r, "img", ret = "src")[0]
-	
 			timestamp = common.parseDOM(r, "p", {'class': 'timestamp'})[0]
 			ts = None
 			if 'TULOSSA' in timestamp:
-				continue;
-
+				continue
 			try:
 				ts = datetime.strptime(timestamp.replace('- ', ''), '%d.%m.%Y %H.%M')
 			except TypeError:
@@ -118,7 +130,6 @@ class katsomo():
 			timestamp = '{:%d.%m.%Y}'.format(ts)
 			playid = urlparse.urlparse(link)[4].split('=')[1]
 			programs.append( {'playid' : playid, 'title':title, 'img' : img } )
-	
 		return programs
 		
 	def getVideoLink(self, link_id):
